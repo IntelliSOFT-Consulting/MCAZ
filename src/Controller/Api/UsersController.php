3c9abd66d0333162ab1c3b86ce826f90be5f3104
Eddie <eddieokemwa@gmail.com>
2018-07-19 09:54:31 +0300

    API's

--- src/Controller/Api/UsersController.php
+++ src/Controller/Api/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends AppController
     public function initialize()
     {
         parent::initialize();
-        $this->Auth->allow(['add', 'token', 'register']);
+        $this->Auth->allow(['add', 'token', 'register', 'forgotPassword']);
     }
 
     /**
