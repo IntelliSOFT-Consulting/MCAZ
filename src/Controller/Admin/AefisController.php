593c317e4447a049f28ab32dea0f966fb0c1b925
zipate <eddieokemwa@gmail.com>
2020-08-01 13:02:19 +0300

    phone

--- src/Controller/Admin/AefisController.php
+++ src/Controller/Admin/AefisController.php
@@ -44,7 +44,7 @@ class AefisController extends AppController
         ]);
 
         if(strpos($this->request->url, 'pdf')) {
-            $this->view$editable = false;Builder()->helpers(['Form' => ['templates' => 'pdf_form',]]);
+            $this->viewBuilder()->helpers(['Form' => ['templates' => 'pdf_form',]]);
             $this->viewBuilder()->options([
                 'pdfConfig' => [
                     'orientation' => 'portrait',
