385297711e5233b31c1116d90627f1077bd1e00b
zipate <eddieokemwa@gmail.com>
2020-09-20 18:08:49 +0300

    Fun; ;

--- config/vigibase_api.php
+++ config/vigibase_api.php
@@ -3,8 +3,8 @@ return [
     /**
      * Vigibase API Keys
      */
-    'vigi_get_url' => 'https://api.who-umc.org/demo/vigibase/icsrs/messagestatus/555235',
-    'vigi_post_url' => 'https://api.who-umc.org/demo/vigibase/icsrs',
+    'vigi_get_url' => 'https://api.who-umc.org/vigibase/icsrs/messagestatus/4',
+    'vigi_post_url' => 'https://api.who-umc.org/vigibase/icsrs',
     'vigi_headers' => ['umc-client-key' => 'a6321b34-1cd6-46e5-af28-5e6edf49bedc',
                           'Authorization' => 'Basic NDUwRUVFN0QtQTQ1Qi00MDNELUE5RTktRjExMENDQ0UzMzdBIA==']
 ];
