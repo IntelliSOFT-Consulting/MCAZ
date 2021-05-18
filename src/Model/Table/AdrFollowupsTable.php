f2c60029790561d0a524f44f30caf7e1bf7dc8ca
zipate <eddieokemwa@gmail.com>
2021-05-18 20:08:35 +0300

    shares

--- src/Model/Table/AdrFollowupsTable.php
+++ src/Model/Table/AdrFollowupsTable.php
@@ -132,221 +132,221 @@ class AdrFollowupsTable extends Table
             ->integer('id')
             ->allowEmpty('id', 'create');
 
-        $validator
-            ->scalar('mrcz_protocol_number')
-            ->notEmpty('mrcz_protocol_number');
-
-        $validator
-            ->scalar('mcaz_protocol_number')
-            ->notEmpty('mcaz_protocol_number');
-
-        $validator
-            ->scalar('principal_investigator')
-            ->notEmpty('principal_investigator');
-
-        $validator
-            ->scalar('reporter_name')
-            ->notEmpty('reporter_name');
-
-        $validator
-            ->scalar('reporter_email')
-            ->notEmpty('reporter_email');
-
-        $validator
-            ->scalar('reporter_phone')
-            ->allowEmpty('reporter_phone');
-
-        $validator
-            ->scalar('name_of_institution')
-            ->notEmpty('name_of_institution');
-
-        $validator
-            ->scalar('institution_code')
-            ->allowEmpty('institution_code');
-
-        $validator
-            ->scalar('study_title')
-            ->notEmpty('study_title');
-
-        $validator
-            ->scalar('study_sponsor')
-            ->allowEmpty('study_sponsor');
+        // $validator
+        //     ->scalar('mrcz_protocol_number')
+        //     ->notEmpty('mrcz_protocol_number');
 
         // $validator
-        //     ->date('date_of_adverse_event')
-        //     ->allowEmpty('date_of_adverse_event');
-
-        $validator
-            ->scalar('participant_number')
-            ->allowEmpty('participant_number');
-
-        $validator
-            ->scalar('report_type')
-            ->allowEmpty('report_type');
+        //     ->scalar('mcaz_protocol_number')
+        //     ->notEmpty('mcaz_protocol_number');
 
         // $validator
-        //     ->date('date_of_site_awareness')
-        //     ->allowEmpty('date_of_site_awareness');
-
-        $validator
-            ->scalar('date_of_birth')
-            ->allowEmpty('date_of_birth');
-
-        $validator
-            ->scalar('gender')
-            ->notEmpty('gender');
-
-        $validator
-            ->scalar('study_week')
-            ->allowEmpty('study_week');
-
-        $validator
-            ->scalar('visit_number')
-            ->allowEmpty('visit_number');
-
-        $validator
-            ->scalar('adverse_event_type')
-            ->allowEmpty('adverse_event_type');
-
-        $validator
-            ->scalar('sae_type')
-            ->notEmpty('sae_type');
-
-        $validator
-            ->scalar('sae_description')
-            ->allowEmpty('sae_description');
-
-        $validator
-            ->scalar('toxicity_grade')
-            ->allowEmpty('toxicity_grade');
-
-        $validator
-            ->scalar('previous_events')
-            ->allowEmpty('previous_events');
-
-        $validator
-            ->scalar('previous_events_number')
-            ->allowEmpty('previous_events_number');
-
-        $validator
-            ->scalar('total_saes')
-            ->allowEmpty('total_saes');
-
-        $validator
-            ->scalar('location_event')
-            ->allowEmpty('location_event');
-
-        $validator
-            ->scalar('location_event_specify')
-            ->allowEmpty('location_event_specify');
-
-        $validator
-            ->scalar('research_involves')
-            ->allowEmpty('research_involves');
-
-        $validator
-            ->scalar('research_involves_specify')
-            ->allowEmpty('research_involves_specify');
-
-        $validator
-            ->scalar('name_of_drug')
-            ->allowEmpty('name_of_drug');
-
-        $validator
-            ->scalar('drug_investigational')
-            ->allowEmpty('drug_investigational');
-
-        $validator
-            ->scalar('patient_other_drug')
-            ->allowEmpty('patient_other_drug');
-
-        $validator
-            ->scalar('report_to_mcaz')
-            ->allowEmpty('report_to_mcaz');
+        //     ->scalar('principal_investigator')
+        //     ->notEmpty('principal_investigator');
 
         // $validator
-        //     ->date('report_to_mcaz_date')
-        //     ->allowEmpty('report_to_mcaz_date');
-
-        $validator
-            ->scalar('report_to_mrcz')
-            ->allowEmpty('report_to_mrcz');
+        //     ->scalar('reporter_name')
+        //     ->notEmpty('reporter_name');
 
         // $validator
-        //     ->date('report_to_mrcz_date')
-        //     ->allowEmpty('report_to_mrcz_date');
-
-        $validator
-            ->scalar('report_to_sponsor')
-            ->allowEmpty('report_to_sponsor');
+        //     ->scalar('reporter_email')
+        //     ->notEmpty('reporter_email');
 
         // $validator
-        //     ->date('report_to_sponsor_date')
-        //     ->allowEmpty('report_to_sponsor_date');
-
-        $validator
-            ->scalar('report_to_irb')
-            ->allowEmpty('report_to_irb');
+        //     ->scalar('reporter_phone')
+        //     ->allowEmpty('reporter_phone');
 
         // $validator
-        //     ->date('report_to_irb_date')
-        //     ->allowEmpty('report_to_irb_date');
+        //     ->scalar('name_of_institution')
+        //     ->notEmpty('name_of_institution');
 
-        $validator
-            ->scalar('medical_history')
-            ->allowEmpty('medical_history');
+        // $validator
+        //     ->scalar('institution_code')
+        //     ->allowEmpty('institution_code');
 
-        $validator
-            ->scalar('diagnosis')
-            ->allowEmpty('diagnosis');
+        // $validator
+        //     ->scalar('study_title')
+        //     ->notEmpty('study_title');
 
-        $validator
-            ->scalar('immediate_cause')
-            ->allowEmpty('immediate_cause');
+        // $validator
+        //     ->scalar('study_sponsor')
+        //     ->allowEmpty('study_sponsor');
 
-        $validator
-            ->scalar('symptoms')
-            ->allowEmpty('symptoms');
+        // // $validator
+        // //     ->date('date_of_adverse_event')
+        // //     ->allowEmpty('date_of_adverse_event');
 
-        $validator
-            ->scalar('investigations')
-            ->allowEmpty('investigations');
+        // $validator
+        //     ->scalar('participant_number')
+        //     ->allowEmpty('participant_number');
 
-        $validator
-            ->scalar('results')
-            ->allowEmpty('results');
+        // $validator
+        //     ->scalar('report_type')
+        //     ->allowEmpty('report_type');
 
-        $validator
-            ->scalar('management')
-            ->allowEmpty('management');
+        // // $validator
+        // //     ->date('date_of_site_awareness')
+        // //     ->allowEmpty('date_of_site_awareness');
 
-        $validator
-            ->scalar('outcome')
-            ->allowEmpty('outcome');
+        // $validator
+        //     ->scalar('date_of_birth')
+        //     ->allowEmpty('date_of_birth');
 
-        $validator
-            ->scalar('d1_consent_form')
-            ->allowEmpty('d1_consent_form');
+        // $validator
+        //     ->scalar('gender')
+        //     ->notEmpty('gender');
 
-        $validator
-            ->scalar('d2_brochure')
-            ->allowEmpty('d2_brochure');
+        // $validator
+        //     ->scalar('study_week')
+        //     ->allowEmpty('study_week');
 
-        $validator
-            ->scalar('d3_changes_sae')
-            ->allowEmpty('d3_changes_sae');
+        // $validator
+        //     ->scalar('visit_number')
+        //     ->allowEmpty('visit_number');
 
-        $validator
-            ->scalar('d4_consent_sae')
-            ->allowEmpty('d4_consent_sae');
+        // $validator
+        //     ->scalar('adverse_event_type')
+        //     ->allowEmpty('adverse_event_type');
 
-        $validator
-            ->scalar('changes_explain')
-            ->allowEmpty('changes_explain');
+        // $validator
+        //     ->scalar('sae_type')
+        //     ->notEmpty('sae_type');
 
-        $validator
-            ->scalar('assess_risk')
-            ->allowEmpty('assess_risk');
+        // $validator
+        //     ->scalar('sae_description')
+        //     ->allowEmpty('sae_description');
+
+        // $validator
+        //     ->scalar('toxicity_grade')
+        //     ->allowEmpty('toxicity_grade');
+
+        // $validator
+        //     ->scalar('previous_events')
+        //     ->allowEmpty('previous_events');
+
+        // $validator
+        //     ->scalar('previous_events_number')
+        //     ->allowEmpty('previous_events_number');
+
+        // $validator
+        //     ->scalar('total_saes')
+        //     ->allowEmpty('total_saes');
+
+        // $validator
+        //     ->scalar('location_event')
+        //     ->allowEmpty('location_event');
+
+        // $validator
+        //     ->scalar('location_event_specify')
+        //     ->allowEmpty('location_event_specify');
+
+        // $validator
+        //     ->scalar('research_involves')
+        //     ->allowEmpty('research_involves');
+
+        // $validator
+        //     ->scalar('research_involves_specify')
+        //     ->allowEmpty('research_involves_specify');
+
+        // $validator
+        //     ->scalar('name_of_drug')
+        //     ->allowEmpty('name_of_drug');
+
+        // $validator
+        //     ->scalar('drug_investigational')
+        //     ->allowEmpty('drug_investigational');
+
+        // $validator
+        //     ->scalar('patient_other_drug')
+        //     ->allowEmpty('patient_other_drug');
+
+        // $validator
+        //     ->scalar('report_to_mcaz')
+        //     ->allowEmpty('report_to_mcaz');
+
+        // // $validator
+        // //     ->date('report_to_mcaz_date')
+        // //     ->allowEmpty('report_to_mcaz_date');
+
+        // $validator
+        //     ->scalar('report_to_mrcz')
+        //     ->allowEmpty('report_to_mrcz');
+
+        // // $validator
+        // //     ->date('report_to_mrcz_date')
+        // //     ->allowEmpty('report_to_mrcz_date');
+
+        // $validator
+        //     ->scalar('report_to_sponsor')
+        //     ->allowEmpty('report_to_sponsor');
+
+        // // $validator
+        // //     ->date('report_to_sponsor_date')
+        // //     ->allowEmpty('report_to_sponsor_date');
+
+        // $validator
+        //     ->scalar('report_to_irb')
+        //     ->allowEmpty('report_to_irb');
+
+        // // $validator
+        // //     ->date('report_to_irb_date')
+        // //     ->allowEmpty('report_to_irb_date');
+
+        // $validator
+        //     ->scalar('medical_history')
+        //     ->allowEmpty('medical_history');
+
+        // $validator
+        //     ->scalar('diagnosis')
+        //     ->allowEmpty('diagnosis');
+
+        // $validator
+        //     ->scalar('immediate_cause')
+        //     ->allowEmpty('immediate_cause');
+
+        // $validator
+        //     ->scalar('symptoms')
+        //     ->allowEmpty('symptoms');
+
+        // $validator
+        //     ->scalar('investigations')
+        //     ->allowEmpty('investigations');
+
+        // $validator
+        //     ->scalar('results')
+        //     ->allowEmpty('results');
+
+        // $validator
+        //     ->scalar('management')
+        //     ->allowEmpty('management');
+
+        // $validator
+        //     ->scalar('outcome')
+        //     ->allowEmpty('outcome');
+
+        // $validator
+        //     ->scalar('d1_consent_form')
+        //     ->allowEmpty('d1_consent_form');
+
+        // $validator
+        //     ->scalar('d2_brochure')
+        //     ->allowEmpty('d2_brochure');
+
+        // $validator
+        //     ->scalar('d3_changes_sae')
+        //     ->allowEmpty('d3_changes_sae');
+
+        // $validator
+        //     ->scalar('d4_consent_sae')
+        //     ->allowEmpty('d4_consent_sae');
+
+        // $validator
+        //     ->scalar('changes_explain')
+        //     ->allowEmpty('changes_explain');
+
+        // $validator
+        //     ->scalar('assess_risk')
+        //     ->allowEmpty('assess_risk');
 
         return $validator;
     }
