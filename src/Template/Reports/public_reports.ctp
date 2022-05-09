81fbe5fb235dce3dcabfe58c27789402cd245c25
itskiprotich <keeprawteachjapheth@gmail.com>
2022-05-09 10:50:16 +0300

    public reports

--- src/Template/Reports/public_reports.ctp
+++ src/Template/Reports/public_reports.ctp
@@ -1,23 +1,48 @@
-
 <?= $this->Html->script('highcharts/highcharts', ['block' => true]); ?>
 <?= $this->Html->script('highcharts/modules/exporting', ['block' => true]); ?>
 <?= $this->Html->script('public_report', ['block' => true]); ?>
 
-<h1 class="page-header text-center"><img alt="" src="/img/report.ico" style="width: 37px;">&nbsp; Reports</h1>
+<h1 class="page-header text-center"><img alt="" src="/img/report.ico" style="width: 37px;">&nbsp; Reports
+</h1>
 
 <div class="row">
     <div class="col-xs-6 col-sm-6">
-      <div id="sadrs-index"></div>
-    </div>    
+        <div id="sadrs-index"></div>
+    </div>
     <div class="col-xs-6 col-sm-6">
-      <div id="aefis-index"></div>
-    </div>    
+        <div id="aefis-index"></div>
+    </div>
 </div>
 <div class="row">
     <div class="col-xs-6 col-sm-6">
-      <div id="adrs-index"></div>
-    </div>    
+        <div id="adrs-index"></div>
+    </div>
     <div class="col-xs-6 col-sm-6">
-      <div id="saefis-index"></div>
-    </div>    
+        <div id="saefis-index"></div>
+    </div>
+</div>
+
+
+<!-- Additional Reports -->
+<div class="row">
+    <div class="col-xs-6 col-sm-6">
+        <div id="year-index"></div>
+    </div>
+    <div class="col-xs-6 col-sm-6">
+        <div id="month-index"></div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12">
+        <div id="province-index"></div>
+    </div>
+
+</div>
+<div class="row">
+    <div class="col-xs-6 col-sm-6">
+        <div id="medicine-index"></div>
+    </div>
+    <div class="col-xs-6 col-sm-6">
+        <div id="institution-index"></div>
+    </div>
 </div>
--- webroot/js/public_report.js
+++ webroot/js/public_report.js
@@ -1,86 +1,144 @@
-$(function () { 
-      
+$(function () {
     function pieChart(data, loc, dname) {
-        // console.log(JSON.stringify(data));        
+        // console.log(JSON.stringify(data));
         var myChart = Highcharts.chart(loc, {
-                chart: {                    
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                        type: 'pie'
-                },
-                title: {
-                    text: data.title
-                },
-                series: [{
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: "pie",
+            },
+            title: {
+                text: data.title,
+            },
+            series: [
+                {
                     data: data.data, //$.map(data.data, function(el) { return el }),//data.data,
-                    name: dname
-                }], 
-                xAxis: {
-                    type: 'category'
-                }
+                    name: dname,
+                },
+            ],
+            xAxis: {
+                type: "category",
+            },
         });
     }
-
 
     function sadrChart(data, loc, dname) {
-        // console.log(JSON.stringify(data));        
+        // console.log(JSON.stringify(data));
         var myChart = Highcharts.chart(loc, {
-                chart: {
-                        type: 'column'
-                },
-                title: {
-                    text: data.title
-                },
-                series: [{
+            chart: {
+                type: "column",
+            },
+            title: {
+                text: data.title,
+            },
+            series: [
+                {
                     data: data.data, //$.map(data.data, function(el) { return el }),//data.data,
-                    name: dname
-                }], 
-                xAxis: {
-                    type: 'category'
-                }
+                    name: dname,
+                },
+            ],
+            xAxis: {
+                type: "category",
+            },
         });
     }
 
     $.ajax({
-        url: '/reports/sadrs-per-designation.json',
-        type: 'GET',
+        url: "/reports/sadrs-per-designation.json",
+        type: "GET",
         async: true,
         dataType: "json",
         success: function (data) {
             console.info(data);
-            sadrChart(data, 'sadrs-index', "Designations");
-        }
+            sadrChart(data, "sadrs-index", "Designations");
+        },
     });
     $.ajax({
-        url: '/reports/aefis-per-designation.json',
-        type: 'GET',
+        url: "/reports/aefis-per-designation.json",
+        type: "GET",
         async: true,
         dataType: "json",
         success: function (data) {
             console.info(data);
-            sadrChart(data, 'aefis-index', "Designations");
-        }
+            sadrChart(data, "aefis-index", "Designations");
+        },
     });
     $.ajax({
-        url: '/reports/adrs-per-designation.json',
-        type: 'GET',
+        url: "/reports/adrs-per-designation.json",
+        type: "GET",
         async: true,
         dataType: "json",
         success: function (data) {
             console.info(data);
-            sadrChart(data, 'adrs-index', "Designations");
-        }
+            sadrChart(data, "adrs-index", "Designations");
+        },
     });
     $.ajax({
-        url: '/reports/saefis-per-designation.json',
-        type: 'GET',
+        url: "/reports/saefis-per-designation.json",
+        type: "GET",
         async: true,
         dataType: "json",
         success: function (data) {
             console.info(data);
-            sadrChart(data, 'saefis-index', "Designations");
-        }
+            sadrChart(data, "saefis-index", "Designations");
+        },
     });
-    
+
+    // Added
+
+    $.ajax({
+        url: "/reports/per-year.json",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        success: function (data) {
+            console.info(data);
+            sadrChart(data, "year-index", "Reports By Year");
+        },
+    });
+
+    $.ajax({
+        url: "/reports/per-month.json",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        success: function (data) {
+            console.info(data);
+            sadrChart(data, "month-index", "Reports By Month");
+        },
+    });
+
+    $.ajax({
+        url: "/reports/per-province.json",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        success: function (data) {
+            console.info(data);
+            sadrChart(data, "province-index", "Reports By Province");
+        },
+    });
+
+    $.ajax({
+        url: "/reports/per-institution.json",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        success: function (data) {
+            console.info(data);
+            sadrChart(data, "institution-index", "Reports By Institution");
+        },
+    });
+
+    $.ajax({
+        url: "/reports/per-medicine.json",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        success: function (data) {
+            console.info(data);
+            sadrChart(data, "medicine-index", "Reports By Medicine");
+        },
+    });
 });
