99464934e425f9e337b4dfed876bcb6c3073a9db
Japheth Kiprotich <jkiprotich@li834-235.members.linode.com>
2022-06-23 06:21:21 +0000

    Email Test File

--- src/Shell/HelloShell.php
+++ src/Shell/HelloShell.php
@@ -2,11 +2,21 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
+use Cake\Mailer\Email;
+use Cake\Utility\Text;
 
 class HelloShell extends Shell
 {
     public function main()
     {
+        
+
+        $this->Email = new Email('default');
+        $this->Email
+            ->emailFormat('html')
+            ->to("jkiprotich@intellisoftkenya.com")
+            ->subject(Text::insert("Test Email", "This is a sample text file: Just a check if the credentials are working well"));
+        $this->Email->send(Text::insert("This is a sample text email: Just a check if the credentials are working well","hey"));
         $this->out('Hello world.');
     }
 }
